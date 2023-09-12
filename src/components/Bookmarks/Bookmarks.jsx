8898b2aca5827080b83160b9603488f2324f2bff
Nahid bin <nahidul15-2728@diu.edu.bd>
2023-09-13 01:44:44 +0600

    creat full

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
 
-const Bookmarks = () => {
+import PropTypes from 'prop-types'
+import Bookmark from '../Bookmark/Bookmark';
+const Bookmarks = ({bookmarks}) => {
     return (
-        <div className='md:w-1/3'>
-            <h2>Bookmarks:</h2>
+        <div className='md:w-1/3 bg-zinc-500 ml-4 mt-3' >
+            <h2 className='text-3xl'>Bookmarked Blog:{bookmarks.length}</h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark> )
+            }
         </div>
     );
 };
+
+Bookmarks.PropTypes = {
+    bookmarks: PropTypes.array
+}
 
 export default Bookmarks;
